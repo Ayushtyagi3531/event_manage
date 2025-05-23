12cc26371383746f1fe4dc1ddf99c72d16dacc60
Ayush Kumar <143243106+Ayushtyagi3531@users.noreply.github.com>
2025-05-23 14:34:57 +0530

    Update settings.py

--- event_manage/settings.py
+++ event_manage/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-08l1lins3i2bz+(15(#zhkp!+)4hpdld^+@bp&-6542o_e1zab"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 import os
 
 # Application definition
